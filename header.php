d7078a4b7b3e50e6fad1aba3360f1e44a4ab70a5
Yannri Zenner <zenner88@gmail.com>
2022-03-09 20:26:30 +0700

    first

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upload Images</title>
+  <link rel="shortcut icon" href="assets/logo-brimob.png" type="image/x-icon">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+</head>
+<body style="background-color: #000000; background-image: linear-gradient(315deg, #000000 0%, #b82e1f 74%);">
---
+++ uploadForm.php
@@ -0,0 +1,14 @@
+<div class="container-fluid" id="uploadForm" style="display: flex; justify-content: center; align-items: center;">
+<div class="wrapper">
+    <div class="text-center">
+    <img src="assets/logo-brimob.png" style="max-width: 100px;" alt="">
+    </div>
+    <form action="#">
+    <input class="file-input" type="file" name="file" accept="image/*" hidden>
+    <i class="fas fa-cloud-upload-alt"></i>
+    <p>Browse Images to Upload</p>
+    </form>
+    <section class="progress-area"></section>
+    <section class="uploaded-area"></section>
+</div>
+</div>
